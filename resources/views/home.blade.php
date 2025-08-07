153d9f8a71650c2813010b7c3e2f8ffc8e770f98
petraalpigiano <petraalpigiano@gmail.com>
2025-08-07 13:03:17 +0200

    add link to others pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <?php
-$greeting = "Hello!"
+$greeting = "Hello!";
+
+use Illuminate\Support\Facades\Route;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,15 +19,15 @@ $greeting = "Hello!"
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">BelNome</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                    <a class="nav-link" href="{{route('features')}}">Features</a>
+                    <a class="nav-link" href="{{route('pricing')}}">Pricing</a>
 
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/features', function () {
+    return view('features');
+})->name('features');
+
+Route::get('/pricing', function () {
+    return view('pricing');
+})->name('pricing');
